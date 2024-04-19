2ab0c16ad9b5f5415da573eec90b5fbe2e13c377
campbellgarth <157370612+campbellgarth@users.noreply.github.com>
2024-04-19 13:00:56 +0200

    removed login-view.scss import from login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';;
 import { useState } from "react";
-import "./login-view.scss"
+
 
 export const LoginView = ({ onLoggedIn }) => {
     const [username, setUsername] = useState("");
